7bc903ccf4bdc4e6eea65f2c2dcd35b630ebddb2
Lukas Himsel <lukas.himsel@web.de>
2017-06-26 19:12:48 +0200

    #21

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,30 +1,3 @@
-var map = L.map('map', { closePopupOnClick: false }).setView(map_coord, 13);
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/">OSM</a>',
-    minZoom: 10
-}).addTo(map);
-
-L.control.fullscreen({
-    position: 'topleft'
-}).addTo(map);
-
-var markers = L.markerClusterGroup({
-    spiderfyOnMaxZoom: true,
-	showCoverageOnHover: false,
-	zoomToBoundsOnClick: true
-});
-coordinates.forEach(function (a) {
-    markers.addLayer(L.marker([a.x, a.y]).on("click", function () {
-        document.location = a.url
-    }).on("mouseover", function(e){
-          this.bindPopup(a.title, {closeButton: false}).openPopup();
-    }).on('mouseout', function (e) {
-          this.closePopup();
-    }));
-});
-map.addLayer(markers);
-
-
 $("form").bind("keypress", function (a) { return 13 == a.keyCode ? !1 : void 0 });
 
 var options = {
@@ -43,6 +16,31 @@ var listObj = new List('list-id', options);
 function showMap() {
     $('#list-id').hide();
     $('#map').show();
+    var map = L.map('map', { closePopupOnClick: false }).setView(map_coord, 13);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/">OSM</a>',
+        minZoom: 10
+    }).addTo(map);
+
+    L.control.fullscreen({
+        position: 'topleft'
+    }).addTo(map);
+
+    var markers = L.markerClusterGroup({
+        spiderfyOnMaxZoom: true,
+        showCoverageOnHover: false,
+        zoomToBoundsOnClick: true
+    });
+    coordinates.forEach(function (a) {
+        markers.addLayer(L.marker([a.x, a.y]).on("click", function () {
+            document.location = a.url
+        }).on("mouseover", function (e) {
+            this.bindPopup(a.title, { closeButton: false }).openPopup();
+        }).on('mouseout', function (e) {
+            this.closePopup();
+        }));
+    });
+    map.addLayer(markers);
 };
 function showList() {
     $('#map').hide();
