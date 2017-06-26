fc217e04c329afaa6e280303ab02e1644717d215
Lukas Himsel <lukas.himsel@web.de>
2017-06-26 19:12:48 +0200

    #21

--- assets/js/list.js
+++ assets/js/list.js
@@ -12,35 +12,40 @@ var options = {
 };
 
 var listObj = new List('list-id', options);
+var initialized = false;
 
 function showMap() {
     $('#list-id').hide();
     $('#map').show();
-    var map = L.map('map', { closePopupOnClick: false }).setView(map_coord, 13);
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/">OSM</a>',
-        minZoom: 10
-    }).addTo(map);
+    if (initialized == false) {
+        var map = L.map('map', { closePopupOnClick: false }).setView(map_coord, 13);
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/">OSM</a>',
+            minZoom: 10
+        }).addTo(map);
 
-    L.control.fullscreen({
-        position: 'topleft'
-    }).addTo(map);
+        L.control.fullscreen({
+            position: 'topleft'
+        }).addTo(map);
 
-    var markers = L.markerClusterGroup({
-        spiderfyOnMaxZoom: true,
-        showCoverageOnHover: false,
-        zoomToBoundsOnClick: true
-    });
-    coordinates.forEach(function (a) {
-        markers.addLayer(L.marker([a.x, a.y]).on("click", function () {
-            document.location = a.url
-        }).on("mouseover", function (e) {
-            this.bindPopup(a.title, { closeButton: false }).openPopup();
-        }).on('mouseout', function (e) {
-            this.closePopup();
-        }));
-    });
-    map.addLayer(markers);
+        var markers = L.markerClusterGroup({
+            spiderfyOnMaxZoom: true,
+            showCoverageOnHover: false,
+            zoomToBoundsOnClick: true
+        });
+        coordinates.forEach(function (a) {
+            markers.addLayer(L.marker([a.x, a.y]).on("click", function () {
+                document.location = a.url
+            }).on("mouseover", function (e) {
+                this.bindPopup(a.title, { closeButton: false }).openPopup();
+            }).on('mouseout', function (e) {
+                this.closePopup();
+            }));
+        });
+        map.addLayer(markers);
+
+        initialized = true;
+    }
 };
 function showList() {
     $('#map').hide();
